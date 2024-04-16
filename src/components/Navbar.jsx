9e7f3faa8972d4ff8126c49eed70f06fc279fa7b
AKKi-28 <akshaymailforwork@gmail.com>
2024-04-16 13:20:24 +0530

    add: initial deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         </div>
         <ul>
           <li className='flex gap-4'>
-            <a className="" href="https://akshayns28.github.io/portfolio/" target="_blank">&#128477;&#65039; Created by <span className='text-green-500 font-semibold hover:font-bold'>AKSHAY N S</span></a>
+            <a className="" href="https://akshayns28.github.io/portfolio/" target="_blank">&#128477;&#65039; Created by <span className='text-green-500 font-semibold hover:font-bold '>AKSHAY N S</span></a>
 
           </li>
         </ul>
